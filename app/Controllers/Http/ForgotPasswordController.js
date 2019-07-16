cc37249c7caa6f3ba2a61e424cd18f516bcee5c5
Iago Carvalho <iagocarvalhoweb@icloud.com>
2019-07-15 22:29:17 -0300

    recuperação de senha

---
+++ app/Controllers/Http/ForgotPasswordController.js
@@ -0,0 +1,65 @@
+'use strict'
+const crypto = require('crypto')
+const moment = require('moment')
+const User = use('App/Models/User')
+const Mail = use('Mail')
+
+class ForgotPasswordController {
+  async store ({ request, response }) {
+    try {
+      const email = request.input('email')
+
+      const user = await User.findByOrFail('email', email)
+      user.token = crypto.randomBytes(10).toString('hex')
+      user.token_created_at = new Date()
+      await user.save()
+
+      await Mail.send(
+        ['emails.forgot_password'],
+        {
+          email,
+          token: user.token,
+          link: `${request.input('redirect_url')}?token=${user.token}`
+        },
+        message => {
+          message
+            .to(user.email)
+            .from('iago@iagocarvalho.com.br')
+            .subject('Recuperação de Senha')
+        }
+      )
+    } catch (err) {
+      return response.status(err.status).send({
+        error: { message: 'Algo não esta certo, esse email existe ?' }
+      })
+    }
+  }
+
+  async update ({ request, response }) {
+    try {
+      const { token, password } = request.all()
+
+      const user = await User.findByOrFail('token', token)
+      const tokenExpired = moment()
+        .subtract('2', 'days')
+        .isAfter(user.token_created_at)
+
+      if (tokenExpired) {
+        return response.status(401).send({
+          error: { message: 'Token expirado !' }
+        })
+      }
+
+      user.token = null
+      user.token_created_at = null
+      user.password = password
+      await user.save()
+    } catch (err) {
+      return response.status(err.status).send({
+        error: { message: 'Algo não errado na sua recuperação de senha' }
+      })
+    }
+  }
+}
+
+module.exports = ForgotPasswordController
--- start/routes.js
+++ start/routes.js
@@ -7,3 +7,5 @@ Route.post('users', 'UserController.store')
 
 // Autenticação
 Route.post('session', 'SessionController.store')
+Route.post('passwords', 'ForgotPasswordController.store')
+Route.put('passwords', 'ForgotPasswordController.update')
