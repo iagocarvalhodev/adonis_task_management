f918880f379cc5a616e2eb43531b99813f2c4b73
Iago Carvalho <iagocarvalhoweb@icloud.com>
2019-07-17 22:40:52 -0300

    especificando rotas

--- start/routes.js
+++ start/routes.js
@@ -7,13 +7,17 @@ Route.post('users', 'UserController.store')
 
 // Autenticação
 Route.post('session', 'SessionController.store')
+// recuperação de senha
 Route.post('passwords', 'ForgotPasswordController.store')
 Route.put('passwords', 'ForgotPasswordController.update')
 
 // upload de arquivo e visualização
 Route.get('files/:id', 'FileController.show')
 
+// rotas que precisam de autenticação
 Route.group(() => {
+  // arquivos
   Route.post('/files', 'FileController.store')
+  // projetos
   Route.resource('projects', 'ProjectController').apiOnly()
 }).middleware(['auth'])
